a69b44b99d9bfefd4eef00c00c97d1f2e63da914
ngctnnnn <ngctnnnn@gmail.com>
2021-10-14 09:19:03 +0700

    fix api

--- landslide/scrape_api.py
+++ landslide/scrape_api.py
@@ -1,6 +1,13 @@
 import requests
 import json
-response = requests.get('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/700000/?key=KC7SPM46W49HDFWL3PX4AP3PX')
+
+# api = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/700000?unitGroup=metric&key="
+lat = '10.762622'
+long = '106.660172'
+api = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + lat + ',' + long + '?key=' 
+api_key = "KC7SPM46W49HDFWL3PX4AP3PX"
+
+response = requests.get(api + api_key)
 df_json = response.json()
 with open('data.json', 'w', encoding='utf-8') as f:
     json.dump(df_json, f, ensure_ascii=False, indent=4)
