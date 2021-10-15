f656b1d7df396dc64de1b01136abfebcb2ef02c1
BTrDung <btridung1621@gmail.com>
2021-10-15 21:21:33 +0700

    convert format from json to csv

--- landslide/json_to_csv_weather.py
+++ landslide/json_to_csv_weather.py
@@ -1,8 +1,53 @@
+import json 
+import numpy as np 
+import pandas as pd 
+
+def push_csv(direct_csv='./data/weather.csv', direct_json='./data/tmp_weather.json'): 
+    file_json_tmp_weather = open('./data/tmp_weather.json', ) 
+    data = json.load(file_json_tmp_weather) 
+    prefix_row = [] 
+    rows = []
+    for primary_key in data: 
+        if primary_key != 'days' and primary_key != 'stations': 
+            prefix_row.append(data[primary_key])
+            pass
+
+        if primary_key == 'days': 
+            for key in data['days'][0]: 
+                if key != 'stations' and key != 'normal': 
+                    prefix_row.append(data['days'][0][key])
+            pass
+
+        if primary_key == 'stations': 
+            for key_station in data['stations']: 
+                row = prefix_row.copy()
+                row.append(key_station)
+
+                for key in data['stations'][key_station]: 
+                    row.append(data['stations'][key_station][key])
+
+                rows.append(row)
+            pass  
+    return rows  
+
+
+def make_name_column(): 
+    name = [ 
+        "queryCost","latitude","longitude","resolvedAddress","address","timezone","tzoffset",
+        "feelslikemax","feelslikemin","feelslike","dew","humidity","precip","precipprob","precipcover",
+        "preciptype","snow","snowdepth","windgust","windspeed","winddir","pressure","cloudcover",
+        "visibility","solarradiation","solarenergy","uvindex","moonphase","conditions","description","icon",
+        "stations", "distance","latitude","longitude", "useCount","id","name","quality","contribution"
+    ]
+    return name
+
 if __name__=='__main__':   
 
     file_json_weather = open('./data/weather.json', 'r') 
-    new_json = [] 
-    cnt = 0 
+    new_json = []
+    table = []  
+
+    table.append(make_name_column())
     for line_json_weather in file_json_weather:  
         if len(line_json_weather) > 0: 
             new_json.append(line_json_weather)
@@ -10,7 +55,7 @@ if __name__=='__main__':
             if line_json_weather[0] == '}':
                 # Open file temp to write each json 
                 file_json_tmp = open('./data/tmp_weather.json', 'w')
-
+                
                 for i in new_json: 
                     file_json_tmp.write(i) 
 
@@ -19,6 +64,11 @@ if __name__=='__main__':
                 # Renew json -> Read next json
                 new_json = [] 
                 
-                cnt += 1
-                if cnt == 2: 
-                    break
+                # Push to csv
+                row_csv = push_csv()
+                
+                for row in row_csv: 
+                    table.append(row)
+              
+
+    pd.DataFrame(table).to_csv("./data/weather.csv", header=None, index=False) 
